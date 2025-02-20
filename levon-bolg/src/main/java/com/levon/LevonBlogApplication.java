95c12045135e36fccbb425825ff76934c534651d
leivik <example@gmial.com>
2025-02-20 16:51:24 +0800

    init

---
+++ levon-bolg/src/main/java/com/levon/LevonBlogApplication.java
@@ -0,0 +1,7 @@
+package com.levon;
+
+public class LevonBlogApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ levon-framework/src/main/java/com/levon/Main.java
@@ -0,0 +1,7 @@
+package com.levon;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
